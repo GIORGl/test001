24660d1c7b1307c2bed823d58b62e4de255451a5
GIORGI <kechekhmadzegiorgi@gmail.com>
2022-07-18 13:48:10 +0400

    first commit

--- initializer.sh
+++ initializer.sh
@@ -3,16 +3,24 @@
 
 # echo $NAME
 
-echo "First arg: $1"
+# echo "First arg: $1"
+# git init
+# git add .
+
+# git commit -m "first commit"
+
+# git branch -M main
+
+
+# git remote add origin "https://github.com/GIORGl/$1.git"
+
+# git push -u origin main
+
+
+
 git init
 git add .
-
 git commit -m "first commit"
-
 git branch -M main
-
-
-git remote add origin "https://github.com/GIORGl/$1.git"
-
+git remote add origin https://github.com/GIORGl/automation-test3.git
 git push -u origin main
-
