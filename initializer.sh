8c12d513fa78b97c859549836d9eee1e7071bb5a
GIORGI <kechekhmadzegiorgi@gmail.com>
2022-07-18 13:46:21 +0400

    first commit

--- initializer.sh
+++ initializer.sh
@@ -11,7 +11,9 @@ git commit -m "first commit"
 
 git branch -M main
 
-git remote add origin https://github.com/GIORGl/$1.git
+
+
+ git remote add origin "https://github.com/GIORGl/$1.git"
 
 git push -u origin main
 
